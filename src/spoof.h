3c8c5f3d2c88f5016e22b0c0ab49bde5461145a1
doomboy00 <henry@webratsmedia.co.uk>
2014-12-09 16:51:36 +0000

    Re-forked and updated

--- src/spoof.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDV.h>
-
-@interface spoof : CDVPlugin
-
-- (void)getMac:(CDVInvokedUrlCommand*)command;
-- (void)listAdd:(CDVInvokedUrlCommand*)command;
-- (void)spoof:(CDVInvokedUrlCommand*)command;
-
-@end
